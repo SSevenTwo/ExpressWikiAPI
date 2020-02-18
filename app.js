39e8d8689900b698efe2dfdf3a3bc15a3585865a
Ian Nguyen <iannguyen172@gmail.com>
2020-02-18 14:48:55 +1100

    Initial app set up

---
+++ app.js
@@ -0,0 +1,16 @@
+//jshint esversion:8
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
